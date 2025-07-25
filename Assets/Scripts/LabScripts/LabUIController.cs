4e6d0b6781935317c25943ee66866b7bd494127b
Guilherme Paes <guy.paes176@gmail.com>
2025-07-25 06:37:50 -0300

    feat: add first logic to SolutionAnimationPanel
    Co-Authored-By: João Guilherme B. A. Rezende <86573978+bennie10colado@users.noreply.github.com>

--- Assets/Scripts/LabScripts/LabUIController.cs
+++ Assets/Scripts/LabScripts/LabUIController.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using TMPro;
 
@@ -5,43 +6,38 @@ namespace LabScripts
 {
     public class UIController : MonoBehaviour
     {
+        public GameObject solutionAnimationPanel;
         public GameObject confirmationPanel;
         public TextMeshProUGUI confirmationPanelText;
 
         [HideInInspector]
         public string currentItemName;
 
+        // Hide the confirmation panel at the start
         void Start()
         {
-            if (confirmationPanel != null)
-            {
-                confirmationPanel.SetActive(false);
-            }
+            confirmationPanel?.SetActive(false);
+            solutionAnimationPanel?.SetActive(false);
         }
 
+        // Confirmation Panel
         public void ShowConfirmationPanel(string itemName)
         {
             currentItemName = itemName;
             confirmationPanelText.text = "Iniciar mistura de solubilidade com " + itemName + "?";
 
-            if (confirmationPanel != null)
-            {
-                confirmationPanel.SetActive(true);
-            }
+            confirmationPanel?.SetActive(true);
         }
 
         public void HideConfirmationPanel()
         {
-            if (confirmationPanel != null)
-            {
-                confirmationPanel.SetActive(false);
-            }
+            confirmationPanel?.SetActive(false);
         }
 
         public void OnConfirmAction()
         {
             Debug.Log("Ação Confirmada para o item: " + currentItemName);
-            HideConfirmationPanel(); // Oculta o painel após a confirmação
+            ShowSolutionAnimationPanel();
         }
 
         public void OnCancelAction()
@@ -49,5 +45,27 @@ namespace LabScripts
             Debug.Log("Ação Cancelada para o item: " + currentItemName);
             HideConfirmationPanel(); // Oculta o painel após o cancelamento
         }
+
+        // Solution Animation Panel
+        public void ShowSolutionAnimationPanel()
+        {
+            solutionAnimationPanel?.SetActive(true);
+
+            HideConfirmationPanel();
+
+            StartCoroutine(WaitAndCloseSolutionPanel());
+        }
+
+        public void HideSolutionAnimationPanel()
+        {
+            solutionAnimationPanel?.SetActive(false);
+        }
+
+        private IEnumerator WaitAndCloseSolutionPanel()
+        {
+            yield return Utils.Wait(7f);
+            HideSolutionAnimationPanel();
+        }
+
     }
 }
